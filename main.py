e3fe5ea4371477814a182e80a4553442595300f2
Prostoyy2008 <116743039+Prostoyy2008@users.noreply.github.com>
2025-02-19 17:46:19 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,27 @@
+import asyncio
+from aiogram import Bot, Dispatcher, types
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram.filters import Command
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+TOKEN = os.getenv("BOT_TOKEN")  # Загружаем токен из .env
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher()
+
+@dp.message(Command("start"))
+async def start_command(message: types.Message):
+    keyboard = InlineKeyboardMarkup(
+        inline_keyboard=[ 
+            [InlineKeyboardButton(text="Открыть mini-app", web_app=types.WebAppInfo(url="https://prostoyy2008.github.io/-/"))]
+        ]
+    )
+    await message.answer("Добро пожаловать! Нажмите кнопку для открытия мини-приложения.", reply_markup=keyboard)
+
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
